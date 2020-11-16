28103b3ae44d98de4ed89ef1a2a899ccca586d4f
CMD <gautamkailashsingh@gmail.com>
2020-11-16 19:50:11 +0530

    [Gautam] Ability to add a new Employee to the EmployeePayroll JSON Server.

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -45,7 +45,6 @@ namespace RestSharpTest
             }
         }
 
-
         [TestMethod]
         public void givenEmployee_OnPost_ShouldReturnAddedEmployee()
         {
@@ -54,17 +53,11 @@ namespace RestSharpTest
             jObjectbody.Add("name", "Clark");
             jObjectbody.Add("Salary", "15000");
             request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
-
-            //act
             IRestResponse response = client.Execute(request);
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
             Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
             Assert.AreEqual("Clark", dataResponse.name);
             Assert.AreEqual(15000, dataResponse.Salary);
-
         }
-
-
-
     }
 }
