d0442a8b1fea1cc0b3b3e8a8dd5c2927ad6a86a6
CMD <gautamkailashsingh@gmail.com>
2020-11-16 22:16:43 +0530

    [Gautam] Ability to add multiple Employee to the EmployeePayroll JSON Server.

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -22,7 +22,7 @@ namespace RestSharpTest
         [TestInitialize]
         public void Setup()
         {
-            client = new RestClient("http://localhost:5000");
+            client = new RestClient("http://localhost:4000");
         }
 
         private IRestResponse getEmployeeList()
@@ -33,7 +33,7 @@ namespace RestSharpTest
         }
 
         [TestMethod]
-        public void onCallingGETApi_ReturnEmployeeList()
+        public void OnCallingGETApi_ReturnEmployeeList()
         {
             IRestResponse response = getEmployeeList();
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
@@ -46,7 +46,7 @@ namespace RestSharpTest
         }
 
         [TestMethod]
-        public void givenEmployee_OnPost_ShouldReturnAddedEmployee()
+        public void GivenEmployee_OnPost_ShouldReturnAddedEmployee()
         {
             RestRequest request = new RestRequest("/employees", Method.POST);
             JObject jObjectbody = new JObject();
@@ -58,6 +58,31 @@ namespace RestSharpTest
             Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
             Assert.AreEqual("Clark", dataResponse.name);
             Assert.AreEqual(15000, dataResponse.Salary);
+        }
+
+        [TestMethod]
+        public void GivenMultipleEmployee_OnPost_ShouldReturnCountOfEmployees()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject []jObjectbody = new JObject[2];
+            JObject obj = new JObject();
+            obj.Add("name", "Baba Ka Dhaba");
+            obj.Add("Salary", "15000");
+            jObjectbody[0] = obj;
+            obj = new JObject();
+            obj.Add("name", "Swad Official");
+            obj.Add("Salary", "16000");
+            jObjectbody[1] = obj;
+            var json = JsonConvert.SerializeObject(jObjectbody);
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+            request.AddJsonBody(jObjectbody);
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Employee []dataResponse = JsonConvert.DeserializeObject<Employee[]>(response.Content);
+            Assert.AreEqual("Baba Ka Dhaba", dataResponse[0].name);
+            Assert.AreEqual(15000, dataResponse[0].Salary);
+            Assert.AreEqual("Swad Official", dataResponse[1].name);
+            Assert.AreEqual(16000, dataResponse[1].Salary);
         }
     }
 }
